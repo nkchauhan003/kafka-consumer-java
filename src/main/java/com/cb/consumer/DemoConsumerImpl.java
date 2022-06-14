57408eb92a0b7d35f43f20abbb54a44f3fe3e029
nkchauhan003 <nkchauhan003@gmail.com>
2022-06-14 18:22:26 +0530

    message

--- src/main/java/com/cb/consumer/DemoConsumerImpl.java
+++ src/main/java/com/cb/consumer/DemoConsumerImpl.java
@@ -13,7 +13,6 @@ import java.util.Properties;
 public class DemoConsumerImpl implements DemoConsumer {
     public static final String KAFKA_SERVER_URL = "localhost";
     public static final int KAFKA_SERVER_PORT = 9092;
-    public static final String CLIENT_ID = "demo-consumer";
     public static final String GROUP_ID = "demo-group";
     private final KafkaConsumer<String, String> consumer;
     private final String topic;
@@ -21,7 +20,7 @@ public class DemoConsumerImpl implements DemoConsumer {
     public DemoConsumerImpl(String topic) {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER_URL + ":" + KAFKA_SERVER_PORT);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, CLIENT_ID);
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
@@ -31,7 +30,7 @@ public class DemoConsumerImpl implements DemoConsumer {
         props.put(
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringDeserializer");
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+
         this.consumer = new KafkaConsumer<>(props);
         this.topic = topic;
     }
